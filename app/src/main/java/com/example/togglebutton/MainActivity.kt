c3a670a058b02e2eb8d8acce818084e583766094
leonCookie <Leongdynia1105@gmail.com>
2021-11-08 15:55:07 +0100

    koniec

--- app/src/main/java/com/example/togglebutton/MainActivity.kt
+++ app/src/main/java/com/example/togglebutton/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.togglebutton
 import android.R.attr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 
 import android.R.attr.data
 import android.graphics.*
-import android.widget.CompoundButton
-import android.widget.ToggleButton
+import android.widget.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,10 +17,12 @@ class MainActivity : AppCompatActivity() {
         lateinit var red: Button
         lateinit var blue: Button
         lateinit var green: Button
+        lateinit var cat: ImageView
 
         red=findViewById<ToggleButton>(R.id.toggleButtonRed)
         blue=findViewById<ToggleButton>(R.id.toggleButtonBlue)
         green=findViewById<ToggleButton>(R.id.toggleButtonGreen)
+        cat = findViewById<ImageView>(R.id.imageCat)
 
         val bitmap = BitmapFactory.decodeResource(resources, R.drawable.cat)
         val bitmapRed = BitmapFactory.decodeResource(resources, R.drawable.red)
@@ -50,6 +49,8 @@ class MainActivity : AppCompatActivity() {
                 canvas.drawARGB(0,0,0,0)
                 canvas.drawBitmap(bitmapRed,null,RectF(0f,0f,bitmap.width.toFloat(),bitmap.height.toFloat()),paint)
                 canvas.drawBitmap(bitmap,0f,0f,paint)
+                cat.setImageBitmap(filterBitmap)
+
 
 
 
@@ -63,6 +64,15 @@ class MainActivity : AppCompatActivity() {
                 red.isChecked = false
                 green.isChecked = false
 
+
+                //magia
+                val paint = Paint()
+                paint.alpha = 100
+                val canvas = Canvas(filterBitmap)
+                canvas.drawARGB(0,0,0,0)
+                canvas.drawBitmap(bitmapBlue,null,RectF(0f,0f,bitmap.width.toFloat(),bitmap.height.toFloat()),paint)
+                canvas.drawBitmap(bitmap,0f,0f,paint)
+                cat.setImageBitmap(filterBitmap)
             } else {
 
             }
@@ -73,6 +83,16 @@ class MainActivity : AppCompatActivity() {
                 blue.isChecked = false
                 red.isChecked = false
 
+
+
+                //magia
+                val paint = Paint()
+                paint.alpha = 100
+                val canvas = Canvas(filterBitmap)
+                canvas.drawARGB(0,0,0,0)
+                canvas.drawBitmap(bitmapGreen,null,RectF(0f,0f,bitmap.width.toFloat(),bitmap.height.toFloat()),paint)
+                canvas.drawBitmap(bitmap,0f,0f,paint)
+                cat.setImageBitmap(filterBitmap)
             } else {
 
             }
